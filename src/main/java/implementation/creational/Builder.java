ce4254212a8d6eb17a01cbaae2a12e15fd954823
trancongduynguyen1997 <tcdnguyen1997@gmail.com>
2021-11-06 08:17:23 +0700

    How to Builder

---
+++ src/main/java/implementation/creational/Builder.java
@@ -0,0 +1,51 @@
+package implementation.creational;
+
+import lombok.Getter;
+
+public class Builder {
+    void main() {
+        Person person = Person.builder().setMale(false).setAge(1).build();
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+    private boolean isMale;
+
+    public Person(PersonBuilder builder) {
+        this.name = builder.getName();
+        this.age = builder.getAge();
+        this.isMale = builder.isMale();
+    }
+
+    @Getter
+    static class PersonBuilder {
+
+        private String name;
+        private int age;
+        private boolean isMale;
+
+        public PersonBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PersonBuilder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public PersonBuilder setMale(boolean male) {
+            isMale = male;
+            return this;
+        }
+        public Person build() {
+            return new Person(this);
+        }
+    }
+
+    static public PersonBuilder builder() {
+        return new PersonBuilder();
+    }
+}
